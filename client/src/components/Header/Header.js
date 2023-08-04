fb3124336089193074e41e85bcefce8e9789fcb4
Chris Seaman <chris@TechEd-0014.techeducators.co.uk>
2023-08-04 15:17:15 +0100

    update

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <h3>qwertyuiop</h3>
+    </header>
+  );
+}
---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <h2>qwertyuiop</h2>
+    </main>
+  );
+}

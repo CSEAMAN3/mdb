85ce0175890812241a1843059aaf425895a3f304
Chris Seaman <chris@TechEd-0014.techeducators.co.uk>
2023-08-07 14:24:00 +0100

    update

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,18 +5,24 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header>
-      <h1>Book Keepers</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
+    <header className="header-header">
+      <div className="container header-container">
+        <h1 className="header-heading bold">Book Keepers</h1>
+        <nav className="header-nav">
+          <ul className="header-nav-list">
+            <li className="header-nav-listItem">
+              <Link className="header-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="header-nav-listItem">
+              <Link className="header-link" to="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
